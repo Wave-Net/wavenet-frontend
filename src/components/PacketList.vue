fcd86b171ce62b4f42ab96195a267189c5a472ea
zhy2on <dhwlgus5940@gmail.com>
2024-05-02 14:14:40 +0900

    feat: 패킷리스트 수정 - 웹소켓에서 받아온 데이터를 사용하도록 변경

--- src/components/PacketList.vue
+++ src/components/PacketList.vue
@@ -3,24 +3,19 @@
         <DataTable 
         id="mytable"
         :value="products" 
-        :size="small" 
+        size="small"
         showGridlines 
         scrollable scrollHeight="100vh"  
         tableStyle="min-width: 50rem"
         contextMenu v-model:contextMenuSelection="selectedProduct"
         >
-            <!-- size : 칸 간격
-            showGridlines : 그리드 속성 
-            scrollable scrollHeight 스크롤바/스크롤바 생성 위
-        -->
-        
             <Column field="number" header="No."></Column>
             <Column field="time" header="Time"></Column>
             <Column field="source" header="Source IP"></Column>
             <Column field="destination" header="Destination IP"></Column>
-            <Column field="proto" header="Protocol"></Column>
-            <Column field="len" header="Len"></Column>
-            <Column field="info" header="Info"></Column>
+            <Column field="protocol" header="Protocol"></Column>
+            <Column field="length" header="Len"></Column>
+            <Column field="info" header="Info"></Column> 
         </DataTable>
     </div>
 </template>
@@ -28,22 +23,44 @@
 <script>
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
-import { packet } from '@/./pktlist/packet';
+import { useWebSocketStore } from '@/store/websocketStore';
 
 export default {
-    components:{
+    components: {
         DataTable,
         Column,
-
     },
     data() {
         return {
-            products: null
+            products: [],
+            selectedProduct: null,
+            websocketStore: useWebSocketStore(),
         };
     },
     mounted() {
-        packet.getProductsMini().then((data) => (this.products = data));
-    }
+        this.websocketStore.connect();
+        this.subscribeToWebSocketStore();
+    },
+    beforeUnmount() {
+        this.websocketStore.disconnect();
+    },
+    methods: {
+        subscribeToWebSocketStore() {
+            this.websocketStore.$subscribe((mutation) => {
+                if (mutation.type === 'websocket/addMessage') {
+                    const newMessage = mutation.payload;
+                    this.products.push({
+                        number: this.products.length + 1,
+                        time: '',
+                        source: '',
+                        destination: '',
+                        protocol: 'MQTT',
+                        length: newMessage.length,
+                        info: newMessage.data,
+                    });
+                }
+            });
+        },
+    },
 };
-
 </script>
