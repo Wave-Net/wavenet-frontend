a2394f01b0d2c3fc98ae62ca541131c6a0b9a8db
ihyeeun <oioigl1002@gamil.com>
2024-05-03 12:59:55 +0900

    chore: components 디렉토리에 파일 추가

--- src/components/MenuButton.vue
+++ src/components/MenuButton.vue
@@ -1,24 +1,12 @@
 <template>
-    <div>
-        <ToggleButton 
-        v-model="capturechecked" 
-        onLabel="중단" 
-        offLabel="시작"
-        onIcon="pi pi-stop-circle" 
-        offIcon="pi pi-play-circle"
-        class="startButton"
-        
-        />
+  <div>
+    <ToggleButton v-model="capturechecked" onLabel="중단" offLabel="시작" onIcon="pi pi-stop-circle"
+      offIcon="pi pi-play-circle" class="startButton" />
 
-        <SplitButton
-        label="데이터내보내기"
-        :model="items"
-        :disabled="capturechecked"
-        class="dataPrint"
-        >
-            <i class="pi pi-download"> 데이터 내보내기</i> <!-- pi-download 아이콘 추가 -->
-        </SplitButton>
-    </div>
+    <SplitButton label="데이터내보내기" :model="items" :disabled="capturechecked" class="dataPrint">
+      <i class="pi pi-download"> 데이터 내보내기</i> <!-- pi-download 아이콘 추가 -->
+    </SplitButton>
+  </div>
 </template>
 
 <script>
@@ -29,39 +17,40 @@ import 'primeicons/primeicons.css'
 
 
 export default {
-    components:{
-        ToggleButton,
-        SplitButton,
-    },
-    data () {
-        return {
-            capturechecked: false,
-            items:[
-                {
-                    label: 'PCAP',
-                },
-                {
-                    label: 'JSON',
-                },
-                {
-                    label: 'CSV'
-                },
-            ],
-        };
-    }
+  components: {
+    ToggleButton,
+    SplitButton,
+  },
+  data() {
+    return {
+      capturechecked: false,
+      items: [
+        {
+          label: 'PCAP',
+        },
+        {
+          label: 'JSON',
+        },
+        {
+          label: 'CSV'
+        },
+      ],
+    };
+  }
 };
 </script>
 
 <style>
-.startButton{
-    margin-top: 5px;
-    margin-left: 5px;
-    margin-bottom: 5px;
+.startButton {
+  margin-top: 5px;
+  margin-left: 5px;
+  margin-bottom: 5px;
 }
-.dataPrint{
-    margin-top: 5px;
-    margin-left: 5px;
-    margin-bottom: 5px;
-    margin-right: 5px;
+
+.dataPrint {
+  margin-top: 5px;
+  margin-left: 5px;
+  margin-bottom: 5px;
+  margin-right: 5px;
 }
 </style>
