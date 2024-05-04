d527d910e0b65a5e02535162d3e947986caeadb1
ihyeeun <oioigl1002@gamil.com>
2024-05-04 13:42:29 +0900

    chore: 필요없는 부분 제거, #3

--- src/components/PacketList.vue
+++ src/components/PacketList.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="card">
-    <ContextMenu ref="cm" :model="menuModel" @hide="selectedProduct = null" />
+    <ContextMenu ref="cm" :model="menuModel" @hide="selectedRow = null" />
     <DataTable :value="messages" id="mytable" size="small" showGridlines scrollable scrollHeight="95vh"
-      tableStyle="min-width: 50rem" contextMenu v-model:contextMenuSelection="selectedProduct"
+      tableStyle="min-width: 50rem" contextMenu v-model:contextMenuSelection="selectedRow"
       @rowContextmenu="onRowContextMenu">
       <Column field="rowIndex" header="No.">
         <template #body="{ index }">
@@ -18,14 +18,13 @@
     </DataTable>
     <Dialog v-model:visible="display" :modal="true" header="패킷 다이어그램" :style="{ width: '50vw' }">
       <!-- 패킷 다이어그램 내용을 여기에 추가하세요 -->
-      <!-- 안녕하세요.<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>메롱 -->
-      <div v-if="selectedProductData">
-        <div>Time: {{ selectedProductData.seconds_since_beginning }}</div>
-        <div>Source IP: {{ selectedProductData.source_ip }}</div>
-        <div>Destination IP: {{ selectedProductData.destination_ip }}</div>
-        <div>Protocol: {{ selectedProductData.protocol }}</div>
-        <div>Len: {{ selectedProductData.length }}</div>
-        <div>Info: {{ selectedProductData.topic }}</div>
+      <div v-if="selectedRowData">
+        <div>Time: {{ selectedRowData.seconds_since_beginning }}</div>
+        <div>Source IP: {{ selectedRowData.source_ip }}</div>
+        <div>Destination IP: {{ selectedRowData.destination_ip }}</div>
+        <div>Protocol: {{ selectedRowData.protocol }}</div>
+        <div>Len: {{ selectedRowData.length }}</div>
+        <div>Info: {{ selectedRowData.topic }}</div>
       </div>
     </Dialog>
   </div>
@@ -41,10 +40,10 @@ import Dialog from 'primevue/dialog';
 
 const websocketStore = useWebSocketStore();
 const cm = ref();
-const selectedProduct = ref();
+const selectedRow = ref();
 const menuModel = ref([
-  { label: '패킷 다이어그램', icon: 'pi pi-objects-column', command: () => viewProduct(selectedProduct) },
-  // {label: '플로우차트', icon: 'pi pi-sliders-h', command: () => deleteProduct(selectedProduct)}
+  { label: '패킷 다이어그램', icon: 'pi pi-objects-column', command: () => viewRow(selectedRow) },
+  // {label: '플로우차트', icon: 'pi pi-sliders-h', command: () => deleteProduct(selectedRow)}
 ]);
 
 // Dialog 표시 상태
@@ -63,22 +62,21 @@ onUnmounted(() => {
 // 메시지 목록 가져오기
 const messages = computed(() => websocketStore.messages);
 
-const selectedProductData = ref(null); // 선택한 행의 데이터를 저장할 ref를 생성합니다.
+const selectedRowData = ref(null); // 선택한 행의 데이터를 저장할 ref를 생성합니다.
 
 const onRowContextMenu = (event) => {
   cm.value.show(event.originalEvent);
 
   //몇번째 인덱스에 위치하는지 찾는 역할로, 각 메세지가 event.data와 동일한지 확인.
-  selectedProduct.value = messages.value.findIndex(message => message === event.data);
-  console.log(selectedProduct.value);
-  selectedProductData.value = messages.value[selectedProduct.value]; // 선택한 행의 데이터를 할당합니다.
+  selectedRow.value = messages.value.findIndex(message => message === event.data);
+  console.log(selectedRow.value);
+  selectedRowData.value = messages.value[selectedRow.value]; // 선택한 행의 데이터를 할당합니다.
 };
 
-const viewProduct = (product) => {
+const viewRow = (rowIndex) => {
   // 여기에 행을 다이어그램화 하는 로직을 추가하세요.
-  // display.value = true;
 
-  if (product !== null) {
+  if (rowIndex !== null) {
     display.value = true;
   } else {
     console.log('행을 선택해주세요.');
