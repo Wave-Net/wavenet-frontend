db91b598b08ff5dad44a56aa33d316e7c7a63f30
lthlth0000 <lthlth0001@gmail.com>
2024-05-06 15:06:16 +0900

    feat: 대시보드 데이터,ㅐ시보드타임데이터 추가

--- src/components/DashboardData.vue
+++ src/components/DashboardData.vue
@@ -1,5 +1,33 @@
 <template>
-    <div>
-        </div>
-        
+  <div>
+    <DataTable :value="products">
+      <Column field="name"></Column>
+      <Column field="price"></Column>
+    </DataTable>
+  </div>
 </template>
+
+<script>
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import 'primevue/resources/themes/saga-blue/theme.css'; // 테마 스타일링
+import 'primevue/resources/primevue.min.css'; // PrimeVue CSS 파일
+import 'primeicons/primeicons.css'; // PrimeIcons CSS 파일
+
+export default {
+  name: 'MyDataTable',
+  components: {
+    DataTable,
+    Column
+  },
+  data() {
+    return {
+      products: [
+        { name: 'Product 1', price: 100, category: 'Category A' },
+        { name: 'Product 2', price: 200, category: 'Category B' },
+        { name: 'Product 3', price: 300, category: 'Category A' }
+      ]
+    };
+  }
+};
+</script>
