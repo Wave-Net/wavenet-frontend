d4eca29025ca44141c89651a35d0782fe037cdc6
ihyeeun <oioigl@naver.com>
2024-05-07 17:26:47 +0900

    chore: Diagram.vue 컴포넌트 파일 추가

---
+++ src/components/PacketDiagram.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-if="selectedRowData">
+    <!-- selectedRowData를 사용하여 대화창 내용을 표시합니다. -->
+    <p> 안녕하세용가리 </p>
+    Len: {{ selectedRowData.length }} <br>
+    qos: {{ selectedRowData.qos }} <br>
+    value: {{ selectedRowData.value }} <br>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    selectedRowData: Object
+  },
+  mounted() {
+    console.log(this.selectedRowData);
+  },
+  // 나머지 코드
+}
+</script>
