6946498e98b46a4f388953ba8800be387a742cc2
ihyeeun <oioigl@naver.com>
2024-05-14 20:43:24 +0900

    feat: dashboard-table 웹소켓 연결

--- src/components/DashboardData.vue
+++ src/components/DashboardData.vue
@@ -1,44 +1,61 @@
 <template>
   <div>
-    <div v-for="(product, index) in products" :key="index">
-      <div class="title-row">{{ product.name }}: {{ product.price }}</div>
+    <div class="title-row">
+      패킷 송신:
+      <span>{{ table_pkt_send }}</span>
+    </div>
+    <div class="title-row">
+      패킷 수신:
+      <span>{{ table_pkt_recv }}</span>
+    </div>
+    <div class="title-row">
+      데이터 송신:
+      <span>{{ table_data_send }}</span>
+    </div>
+    <div class="title-row">
+      데이터 수신:
+      <span>{{ table_data_recv }}</span>
     </div>
   </div>
 </template>
 
 <script>
-import { defineComponent, ref, watch } from "vue";
 import { useWebSocketStore } from "../store/websocketStore";
+import { ref, watch } from "vue";
 
-export default defineComponent({
-  name: "MyDataTable",
+export default {
   setup() {
-    const store = useWebSocketStore();
-    const products = ref([
-      { name: "패킷 출력 횟수", price: 0, category: "Category A" },
-      { name: "패킷 입력 횟수", price: 0, category: "Category B" },
-      { name: "데이터 송신 크기", price: 0, category: "Category A" },
-      { name: "데이터 수신 크기", price: 0, category: "Category A" },
-    ]);
+    const websocketStore = useWebSocketStore();
 
-    // WebSocket 리스너 설정
+    // 각각의 패킷 및 데이터 송/수신 정보를 저장하는 ref를 생성합니다.
+    const table_pkt_recv = ref(0);
+    const table_pkt_send = ref(0);
+    const table_data_recv = ref(0);
+    const table_data_send = ref(0);
+
+    // 웹소켓 스토어의 statMessage가 변경될 때마다 실행되는 watch 함수를 설정합니다.
     watch(
-      () => store.totalStatics,
-      (newValue, oldValue) => {
-        if (newValue !== oldValue) {
-          // 새로운 데이터가 도착하면 products 배열을 업데이트
-          Object.keys(newValue).forEach((key, index) => {
-            products.value[index].price = newValue[key];
-          });
+      () => websocketStore.statMessage,
+      (newValue) => {
+        // 새로운 값이 있고, 그 값에 total_statistics가 있는 경우
+        if (newValue && newValue.total_statistics) {
+          table_pkt_recv.value = newValue.total_statistics.send_pkt;
+          table_pkt_send.value = newValue.total_statistics.recv_pkt;
+          table_data_recv.value = newValue.total_statistics.send_data;
+          table_data_send.value = newValue.total_statistics.recv_data;
         }
       }
     );
 
+    // 각각의 ref를 반환하여 템플릿에서 사용할 수 있게 합니다.
     return {
-      products,
+      table_pkt_recv,
+      table_pkt_send,
+      table_data_recv,
+      table_data_send,
     };
   },
-});
+};
 </script>
 
 <style>
