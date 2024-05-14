b9fff4beee72aba29a1a31142a4108f48e281810
lthlth0000 <lthlth0001@gmail.com>
2024-05-14 16:28:09 +0900

    chore:스타일 수정

--- src/page/flowchart-page.vue
+++ src/page/flowchart-page.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="timeline-container">
       <div class="left-timeline">
-        <Timeline :value="leftEvents">
-          <template #opposite="slotProps">
-            <small class="p-text-secondary">{{ slotProps.item.date }}</small>
-          </template>
-        </Timeline>
-      </div>
+  <Timeline class="left-timeline" :value="leftEvents">
+    <template #opposite="slotProps">
+      <div class="p-text-secondary">{{ slotProps.item.date }}</div>
+    </template>
+  </Timeline>
+</div>
   
       <!-- 추가된 가로선과 텍스트 -->
       <div class="timeline-space">
@@ -18,9 +18,8 @@
       <!-- 추가된 가로선과 텍스트 -->
   
       <div class="right-timeline">
-        <Timeline :value="rightEvents">
-        </Timeline>
-      </div>
+  <Timeline class="right-timeline" :value="rightEvents"></Timeline>
+</div>
     </div>
   </template>
   
@@ -58,24 +57,63 @@
     justify-content: space-between;
   }
 
+  .left-timeline >>> .p-timeline-event-opposite {
+  padding-right: 20px;
+  padding-left:20px;
+}
+
+.left-timeline >>> .p-timeline-event-content {
+  display: none;
+}
+
+.right-timeline >>> .p-timeline-event-opposite {
+  display:none;
+}
+
   .timeline-space {
     flex: 1;
     align-items: center;
     width:100%;
   }
-  
-
 
   .horizontal-line {
-    width: 100%;
-    border-top: 1px solid black; /* 가로선의 스타일 설정 */
-    margin-bottom: 5px; /* 가로선과 텍스트 사이의 간격 조절 */
-  }
+  width: 100%;
+  border-top: 1px solid black;
+  margin-bottom: 5px;
+  position: relative; /* 새로 추가된 속성 */
+  z-index: 1; /* 추가 */
+}
+
+.horizontal-line::before {
+  content: "";
+  position: absolute;
+  right: -10px; /* 화살표 머리 위치 조정 */
+  top: -6px; /* 화살표 머리 위치 조정 */
+  border-top: 6px solid transparent;
+  border-bottom: 6px solid transparent;
+  border-left: 10px solid black; /* 화살표 머리 색상 */
+  z-index: 2; /* 추가 */
+}
+
+.horizontal-line::after {
+  content: "";
+  position: absolute;
+  left: -10px; /* 화살표 머리 위치 조정 */
+  top: -6px; /* 화살표 머리 위치 조정 */
+  border-top: 6px solid transparent;
+  border-bottom: 6px solid transparent;
+  border-right: 10px solid black; /* 화살표 머리 색상 */
+  z-index: 2; /* 추가 */
+}
   
   .horizontal-line-text {
     font-size: 12px; /* 텍스트의 사이즈 설정 */
     margin: 0; /* 디폴트 마진 제거 */
-    margin-bottom: 39px; /* 텍스트 아래쪽 여백 설정 */
+    margin-bottom: 48px; /* 텍스트 아래쪽 여백 설정 */
     text-align: center;
   }
+
+
+
+
   </style>
