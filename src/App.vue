343bce662b23db139d0cb56bf3165823a6115242
ihyeeun <oioigl@naver.com>
2024-05-23 20:56:57 +0900

    chore: 주석삭제/배경색 추가

--- src/App.vue
+++ src/App.vue
@@ -16,35 +16,3 @@ onUnmounted(() => {
   websocketStore.disconnect();
 });
 </script>
-
-<style scoped>
-.app-wavenet {
-  display: flex;
-  flex-direction: column;
-  background-color: #f8fafc;
-  height: 100%;
-}
-header {
-  display: flex;
-  flex-direction: row;
-  height: 50px;
-}
-main {
-  flex: 1;
-}
-footer {
-  height: 21.6px;
-}
-.logo-container {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  height: 50px;
-}
-.logo {
-  width: 50px;
-  height: 50px;
-  margin-left: 5px;
-  margin-right: 5px;
-}
-</style>
