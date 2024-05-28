6335071643469550fb0e8d26069419629452aa8b
zhy2on <52701529+zhy2on@users.noreply.github.com>
2024-05-29 03:41:10 +0900

    feat: 레이아웃 조정

--- src/pages/CapturePage.vue
+++ src/pages/CapturePage.vue
@@ -1,31 +1,23 @@
 <template>
   <TheHeader />
   <main class="content">
-    <Splitter style="width: 100%; height: 100%">
-      <SplitterPanel class="flex align-items-center justify-content-center" :size="15">
+    <Splitter id="splitter-1" style="width: 100%; height: 640px;">
+      <SplitterPanel id="splitter-1-panel-1" class="flex align-items-center justify-content-center p-1" :size="15">
         Panel 1
       </SplitterPanel>
-      <SplitterPanel class="flex align-items-center justify-content-center" :size="85">
-        <Splitter style="width: 100%; height: 100%">
+      <SplitterPanel id="splitter-1-panel-2" class="flex align-items-center justify-content-center" :size="85">
+        <Splitter id="splitter-2" style="width: 100%; height: 100%">
           <SplitterPanel class="flex align-items-center justify-content-center" :size="80">
             <Splitter style="width: 100%; height: 100%" layout="vertical">
-              <SplitterPanel
-                class="flex align-items-center justify-content-center" :size="70"
-              >
+              <SplitterPanel class="flex align-items-center justify-content-center" :size="70">
                 Panel 2
               </SplitterPanel>
-              <SplitterPanel
-                class="flex align-items-center justify-content-center" :size="30"
-              >
+              <SplitterPanel class="flex align-items-center justify-content-center" :size="30">
                 <Splitter style="width: 100%; height: 100%">
-                  <SplitterPanel
-                    class="flex align-items-center justify-content-center"
-                  >
+                  <SplitterPanel class="flex align-items-center justify-content-center">
                     Panel 3
                   </SplitterPanel>
-                  <SplitterPanel
-                    class="flex align-items-center justify-content-center"
-                  >
+                  <SplitterPanel class="flex align-items-center justify-content-center">
                     Panel 4
                   </SplitterPanel>
                 </Splitter>
